c402f83796599f82bb562abc3b733af09d2473e6
sindhurv <sindhurv@cisco.com>
2018-06-28 15:51:29 +0530

    Added documentation

--- code.py
+++ code.py
@@ -1,9 +1,14 @@
+"""
+From the gutenberg corpus available in nltk we take the works of 3 authors.
+For each work we build and train a Unigram, Bigram and a Trigram model
+Given a piece of test text, this code calculates the probability as to which one of the 3 Authors it belongs to.
+It can also generate text similar to the work of a given author.
+"""
 from nltk.corpus import gutenberg
 from itertools import chain
 from math import log
 
-from nltk.probability import (FreqDist,
-    ConditionalProbDist,
+from nltk.probability import (FreqDist, ConditionalProbDist,
     ConditionalFreqDist,
     LidstoneProbDist)
 from nltk.util import ngrams
@@ -11,51 +16,59 @@ from nltk.model.api import ModelI
 from nltk.model.ngram import NgramModel
 from random import randint
 from nltk.tokenize import word_tokenize
+import numpy as np
 
 est = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
 
+noOfFiles = 3
+fileids = ['bryant-stories.txt', 'carroll-alice.txt', 'shakespeare-hamlet.txt']
+Authors = ['Bryant', 'Carroll', 'Shakespeare']
+lenFirstSent = [len(gutenberg.sents(fileids[i])[0])-1 for i in range(noOfFiles)]
+C = [gutenberg.words(fileids[i])[lenFirstSent[i]:] for i in range(noOfFiles)]
+lenC = [len(C[i]) for i in range(noOfFiles)]
+
+unigram = [NgramModel(1, C[i], estimator=est) for i in range(noOfFiles)]
+bigram = [NgramModel(2, C[i], True, True, estimator=est) for i in range(noOfFiles)]
+trigram = [NgramModel(3, C[i], True, True, estimator=est) for i in range(noOfFiles)]
 
 
-noOfFiles=3
-fileids=['bryant-stories.txt','carroll-alice.txt','shakespeare-hamlet.txt']
-lenFirstSent=[len(gutenberg.sents(fileids[i])[0])-1 for i in range(noOfFiles)]
-C=[gutenberg.words(fileids[i])[lenFirstSent[i]:] for i in range(noOfFiles)]
-lenC=[len(C[i]) for i in range(noOfFiles)]
+def generateText(model, train):
+    pos = []
+    for i in range(20):
+        pos.append(train[randint(0, len(train))])
+    return model.generate(50, set(pos))
 
-unigram=[NgramModel(1,C[i],estimator=est) for i in range(noOfFiles)]
-bigram=[NgramModel(2,C[i],True,True,estimator=est) for i in range(noOfFiles)]
-trigram=[NgramModel(3,C[i],True,True,estimator=est) for i in range(noOfFiles)]
-
-def generateText(model,train):
-	pos=[]	
-	for i in range(20):
-		pos.append(train[randint(0,len(train))])
-	return model.generate(50,set(pos))
 
 def modelEntropy():
-	for i in range(noOfFiles):
-		text=generateText(trigram[i],C[i])
-		print fileids[i],':'
-		print unigram[i].entropy(text)
-		print bigram[i].entropy(text)
-		print trigram[i].entropy(text)
+    for i in range(noOfFiles):
+        text = generateText(trigram[i],C[i])
+        print(fileids[i], ':')
+        print(unigram[i].entropy(text))
+        print(bigram[i].entropy(text))
+        print(trigram[i].entropy(text))
+
 
 def identifyAuthor(text):
-	words=word_tokenize(text)
-	for i in range(noOfFiles):
-		my_logprob=[0.0,0.0,0.0]			
-		my_logprob[0]=(-1)*unigram[i].entropy(words)*len(words)	
-		my_logprob[1]=(-1)*bigram[i].entropy(words)*len(words)
-		my_logprob[2]=(-1)*trigram[i].entropy(words)*len(words)
-		print fileids[i],': Unigram: ',my_logprob[0],' Bigram: ',my_logprob[1],' Trigram: ',my_logprob[2]
+    words = word_tokenize(text)
+    logprob = np.zeros([3, 3])
+    for i in range(noOfFiles):
+        logprob[i] = [(-1)*unigram[i].entropy(words)*len(words),
+                        (-1)*bigram[i].entropy(words)*len(words),
+                            (-1)*trigram[i].entropy(words)*len(words)]
+
+    print("Author as predicted by the Unigram model: ", Authors[[i for i in range(3) if logprob[i, 0] == max(logprob[:,0])][0]])
+    print("Author as predicted by the Bigram model: ", Authors[[i for i in range(3) if logprob[i, 1] == max(logprob[:,1])][0]])
+    print("Author as predicted by the Trigram model: ", Authors[[i for i in range(3) if logprob[i, 2] == max(logprob[:,2])][0]])
+
 
 def generateRandomText(i):
-	print "unigrams \n"," ".join(generateText(unigram[i],C[i]))	
-	print "bigrams \n"," ".join(generateText(bigram[i],C[i]))
-	print "trigrams \n"," ".join(generateText(trigram[i],C[i]))
+    print("unigram: \n", " ".join(generateText(unigram[i], C[i])))
+    print("bigram: \n", " ".join(generateText(bigram[i], C[i])))
+    print("trigram: \n", " ".join(generateText(trigram[i], C[i])))
+
 
 def stats():
-	for i in range(noOfFiles):
-		print fileids[i],':'
-		print 'Number of types: ',len(C[i])
-		print 'Number of tokens: ',len(set(C[i]))
+    for i in range(noOfFiles):
+        print(fileids[i], ':')
+        print('Number of types: ', len(C[i]))
+        print('Number of tokens: ', len(set(C[i])))
--- menu.py
+++ menu.py
@@ -1,20 +1,23 @@
-import code
-s=code
-n=1
-while(n>=1 and n<=4):
-	n=input("Enter your choice: \n 1: Display cross entropy values \n 2: Generate text \n 3: Identify author \n 4: Print number of types and tokens\n")
-	n=int(n)
-	if n==1:
-	    s.modelEntropy()
-	    
-	elif n==2:
-	    a=input("Enter story number \n0. Bryant \n 1. Carroll \n 2. Shakespeare \n")  
-	    s.generateRandomText(a)
+"""
+This code is the front end of a Text generation and Authorship estimation project
+Run this code and enter a number based on the functionality you desire
+"""
+import code as s
+n = 1
+while n>=1 and n<=4 :
+    n = input("Enter your choice: \n 1: Display cross entropy values \n 2: Generate text \n 3: Identify author \n 4: Print number of types and tokens\n")
+    n = int(n)
+    if n == 1:
+        s.modelEntropy()
+        
+    elif n == 2:
+        a = input("Enter story number \n0. Bryant \n 1. Carroll \n 2. Shakespeare \n")  
+        s.generateRandomText(a)
 
-	elif n==3:
-		text=input()
-		s.identifyAuthor(text)
+    elif n == 3:
+        text = input()
+        s.identifyAuthor(text)
 
-	elif n==4:
-	    s.stats()
+    elif n == 4:
+        s.stats()
 
